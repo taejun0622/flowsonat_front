e9444e1d791d9cd7f309eef570f482ef920ce149
taejun <dugudugu0622@gmail.com>
2025-09-04 16:17:25 +0900

    feat: version check api upgrade

---
+++ src/api/models/DownloadInfo.ts
@@ -0,0 +1,15 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type DownloadInfo = {
+    platform: string;
+    arch: string;
+    version: string;
+    filename: string;
+    url: string;
+    size: number;
+    checksum: string;
+    buildTime: string;
+};
+
